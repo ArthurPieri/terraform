0fa6b2109fb9de0eac35ef9a59815b0c1343a4b6
arthurpieri <services+github@arthurpieri.com>
2022-07-27 17:23:52 -0300

    feat: dependencia entre resource

--- main.tf
+++ main.tf
@@ -34,3 +34,35 @@ resource "aws_security_group" "allow_ssh" {
     "Name" = "allow-ssh"
   }
 }
+resource "aws_instance" "dev4" {
+  ami           = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
+  key_name      = "terraform-aws"
+  tags = {
+    "Name" = "dev4"
+  }
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+  depends_on = [
+    aws_s3_bucket.dev4
+  ]
+}
+
+resource "aws_instance" "dev5" {
+  ami           = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
+  key_name      = "terraform-aws"
+  tags = {
+    "Name" = "dev5"
+  }
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
+}
+
+resource "aws_s3_bucket" "dev4" {
+  bucket = "teste-dev4"
+  acl    = "private"
+
+  tags = {
+    "Name"      = "teste-dev4"
+    Environment = "dev"
+  }
+}
