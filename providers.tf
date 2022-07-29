cba6769133c7fffee59be2689df446bcaeeb06a8
arthurpieri <services+github@arthurpieri.com>
2022-07-29 14:12:08 -0300

    Changes to be committed: 	deleted:    .terraform.lock.hcl 	deleted:    database.tf 	deleted:    instances.tf 	deleted:    providers.tf 	deleted:    s3-buckets.tf 	deleted:    security-group.tf 	deleted:    terraform-aws.pub

--- providers.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-provider "aws" {
-  alias  = "us-east-2"
-  region = "us-east-2"
-}
--- s3-buckets.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "dev4" {
-  bucket = "teste-dev4"
-  acl    = "private"
-
-  tags = {
-    "Name"      = "teste-dev4"
-    Environment = "dev"
-  }
-}
